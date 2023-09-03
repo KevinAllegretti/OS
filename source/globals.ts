de8aa72d6b0638759bd937320c823b84f08278f1
kevin.allegretti1@marist.edu <Kevinallegretti17>
2023-09-03 15:33:16 -0400

    pip boy 3000

--- source/globals.ts
+++ source/globals.ts
@@ -12,7 +12,7 @@
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
 const APP_NAME: string    = "OS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "2112";   // What did you expect?
+const APP_VERSION: string = "Pip Boy 3000";   // What did you expect?
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
